9b7393e5361b7c2be1579f014f14867d251148b8
MrAntoniS <0shkiper0@gmail.com>
2020-02-17 21:17:15 +0300

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,27 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte numberOfCores = 4;
+        System.out.println(numberOfCores);
+
+        short windows = 10;
+        System.out.println(windows);
+
+        int ssd = 500; // GB
+        System.out.println(ssd);
+
+        long hdd = 1000; // GB
+        System.out.println(hdd);
+
+        float cpuFrequency = 2.30f;
+        System.out.println(cpuFrequency);
+
+        double ram = 8.00;
+        System.out.println(ram);
+
+        char bluetooth = '+';
+        System.out.println(bluetooth);
+        
+        boolean laptop = true;
+        System.out.println(laptop);
+    }
+}
