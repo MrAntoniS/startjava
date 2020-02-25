3ae495d2aa2f7a88a4f3f7d7cb151994447be85e
MrAntoniS <0shkiper0@gmail.com>
2020-02-25 23:17:29 +0300

    implement HW_1.8

--- Lesson_1/GuessNumber.java
+++ Lesson_1/GuessNumber.java
@@ -1,26 +1,48 @@
+//public class GuessNumber {
+//    public static void main(String[] args) {
+//        int myNum = 40;// значение игрока
+//        int compNum = 50;// число от 0 до 100, которое загадал компьютер
+
+//        if(myNum < compNum) {
+//            for(int i = myNum; i <= compNum; i++) {
+//                if(i < compNum) {
+//                    System.out.println(i + " Введенное Вами число меньше того что загадал компьютер." + " Компьютер выиграл!");
+//                } else if(i == compNum) {
+//                    System.out.println(i + " Вы угадали!" + " Вы выиграли!");
+//                }
+//            }
+//        } else if(myNum > compNum) {
+//            for(int i = myNum; i >= compNum; i--) {
+//                if(i > compNum) {
+//                    System.out.println(i + " Введенное Вами число больше того что загадал компьютер." + " Компьютер выиграл!");
+//                } else if(i == compNum) {
+//                    System.out.println(i + " Вы угадали!" + " Вы выиграли!");
+//                }
+//            }
+//        } else if(myNum == compNum) {
+//            System.out.println(myNum + " Вы угадали!" + " Вы выиграли!");
+//        }
+//    }
+//}
+
+
 public class GuessNumber {
     public static void main(String[] args) {
-        int myNum = 40;// значение игрока
+        int myNum = 60;// значение игрока
         int compNum = 50;// число от 0 до 100, которое загадал компьютер
+        int i;
 
-        if(myNum < compNum) {
-            for(int i = myNum; i <= compNum; i++) {
-                if(i < compNum) {
-                    System.out.println(i + " Введенное Вами число меньше того что загадал компьютер." + " Компьютер выиграл!");
-                } else if(i == compNum) {
-                    System.out.println(i + " Вы угадали!" + " Вы выиграли!");
-                }
+        for(i = myNum; i != compNum; i += 0) {
+            if(i < compNum) {
+                System.out.println(i + " Введенное Вами число меньше того что загадал компьютер." + " Компьютер выиграл!");
+                i++;
+            } else if(i > compNum) {
+                System.out.println(i + " Введенное Вами число больше того что загадал компьютер." + " Компьютер выиграл!");
+                i--;
             }
-        } else if(myNum > compNum) {
-            for(int i = myNum; i >= compNum; i--) {
-                if(i > compNum) {
-                    System.out.println(i + " Введенное Вами число больше того что загадал компьютер." + " Компьютер выиграл!");
-                } else if(i == compNum) {
-                    System.out.println(i + " Вы угадали!" + " Вы выиграли!");
-                }
-            }
-        } else if(myNum == compNum) {
-            System.out.println(myNum + " Вы угадали!" + " Вы выиграли!");
+        }
+        if(i == compNum) {
+            System.out.println(i + " Вы угадали!" + " Вы выиграли!");
         }
     }
 }
