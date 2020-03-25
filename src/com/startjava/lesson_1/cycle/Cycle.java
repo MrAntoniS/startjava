1e07e163dd0d48a95af3ff16d0a9cafee9665099
MrAntoniS <0shkiper0@gmail.com>
2020-03-25 22:39:17 +0300

    implement HW_3

--- src/com/startjava/lesson_1/cycle/Cycle.java
+++ src/com/startjava/lesson_1/cycle/Cycle.java
@@ -1,21 +1,23 @@
+package com.startjava.lesson_1.cycle;
+
 public class Cycle {
     public static void main(String[] args) {
         for(int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
 
-        int i = -6;
-        while(i <= 6) {
-            System.out.println(i);
-            i += 2;
+        int k = -6;
+        while(k <= 6) {
+            System.out.println(k);
+            k += 2;
         }
 
-        int i = 13;
+        int z = 13;
         int sumOdd = 11;
         do {
-            sumOdd = sumOdd + i;
-            i += 2;
-        while(i <= 20);
+            sumOdd = sumOdd + z;
+            z += 2;
+        } while(z <= 20);
        System.out.println(sumOdd);
     }
 }
